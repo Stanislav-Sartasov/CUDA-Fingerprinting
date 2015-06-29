0eb12cc4506a3c6f675d543fca58a941b69946ff
Tatyana Zabelina <zzabelinka@gmail.com>
2015-06-29 20:43:23 +0400

    - restore order with variables names

---
+++ Code/CUDAFingerprinting.Common.OrientationField.Test/OrientationFieldTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CUDAFingerprinting.Common;
+using CUDAFingerprinting.OrientationField;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CUDAFingerprinting.Common.OrientationField.Test
+{
+	[TestClass]
+	class OrientationFieldTest
+	{
+		[TestMethod]
+		public static void DrawLine(Bitmap image, int x1, int y1, int x2, double angle)				// рисует прямую линию на изображении от точки (x1, y1) до точки (x2, y2)
+		{
+			int length = x2 - x1;
+			double slope = Math.Tan(angle);
+			for (int x = x1; x < x2; x++)
+			{
+				int y = (int)(x * slope);
+				if (y >= 0 && y < image.Height)
+				{
+					image.SetPixel(x, y - 9000, Color.Black);
+				}
+			}
+		}
+
+		[TestMethod]
+		public static void Visualization(Bitmap sourceImage, CUDAFingerprinting.OrientationField.OrientationField Field)		// визуализирует поле направлений в картинку, где на каждом блоке 16х16 находится отрезок, наклоненный под соответствующим углом
+		{
+			const int SIZE = 16;
+			Bitmap orientedImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+			Graphics g = Graphics.FromImage(orientedImage);
+			g.Clear(Color.White);
+
+			// перебираем все блоки 16х16
+			for (int row = 0; row < SIZE; row++)
+			{
+				for (int column = 0; column < SIZE; column++)
+				{
+					// в каждом блоке получаем направление и строим отрезок
+					double angle = Field.Blocks[row, column].Orientation;
+					if (angle > 0)			// прямая возрастает
+					{
+						// начинаем строить линию с точки (SIZE * column, SIZE * row + SIZE - 1)  -- с нижней левой точки блока
+						DrawLine(orientedImage, SIZE * column, SIZE * row + SIZE - 1, SIZE * (column + 1), angle);
+						orientedImage.Save(@"Resources\1_1_oriented.jpg");
+
+					}
+					else if (angle < 0)		// прямая убывает
+					{
+						// начинаем строить линию с точки (SIZE * column, SIZE * row)  -- с верхней левой точки блока
+						DrawLine(orientedImage, SIZE * column, SIZE * row, SIZE * (column + 1), angle);
+						orientedImage.Save(@"Resources\1_1_oriented.jpg");
+
+					}
+					else					// прямая горизонтальна
+					{
+						// начинаем строить линию с точки (SIZE * column, SIZE * row + SIZE / 2)
+						DrawLine(orientedImage, SIZE * column, SIZE * row + SIZE / 2, SIZE * (column + 1), angle);
+						orientedImage.Save(@"Resources\1_1_oriented.jpg");
+
+					}				
+				}
+			}
+			orientedImage.Save(@"Resources\1_1_oriented.jpg");
+		}
+
+		[TestMethod]
+		public void MainMethod()
+		{
+			Bitmap image;
+			try
+			{  // Retrieve the image.
+				image = new Bitmap(@"Resources\1_1.tif");
+			}
+			catch (ArgumentException)
+			{
+				Console.WriteLine("There was an error. Check the path to the image file.");
+				image = null;
+			}
+			int[,] bytes = new int[image.Width, image.Height];
+			ImageHelper.SaveIntArray(bytes, @"Resources\1_1.tif");
+
+			CUDAFingerprinting.OrientationField.OrientationField FingerPrint = new CUDAFingerprinting.OrientationField.OrientationField(bytes);
+			for (int x = 0; x < image.Width; x += 16)
+			{
+				Console.WriteLine(x / 16 + "\t" + FingerPrint.GetOrientation(x, 0) * 180 / Math.PI);
+			}
+
+			Visualization(image, FingerPrint);
+		}
+	}
+}
--- Code/CUDAFingerprinting.Common.OrientationField/OrientationField.cs
+++ Code/CUDAFingerprinting.Common.OrientationField/OrientationField.cs
@@ -6,17 +6,39 @@ using System.Text;
 
 namespace CUDAFingerprinting.OrientationField
 {
-    public class Block16
+    public class Block
     {
-        public const int SIZE = 16;
-        public double[,] pxl;			// пиксели блока
-		public double orientation;		// массив направлений
-        double[,] Gx;					// градиенты
-        double[,] Gy;
+        private int _size;
+        private double[,] _pxl;			// пиксели блока
+		private double _orientation;		// массив направлений
 
-        public Block16()
+		public double[,] Pxl
+		{
+			get { return _pxl; }
+		}
+		public double Orientation
+		{
+			get { return _orientation; }
+		}
+		public int Size
+		{
+			get { return _size; }
+		}
+
+        public Block(int SIZE, int[,] bytes, int i, int j)		// параметры: размер блока; массив пикселей, из которого копировать в блок; координаты, с которых начать копирование
         {
-            this.pxl = new double[SIZE, SIZE];
+			this._size = SIZE;
+            this._pxl = new double[SIZE, SIZE];
+			// копируем в блоки части массива
+			for (int x = 0; x < SIZE; x++)
+			{
+				for (int y = 0; y < SIZE; y++)
+				{
+					this._pxl[x, y] = bytes[x + i, y + j];
+				}
+			}
+			// вычисляем направления
+			this.SetOrientation();
         }
 
         public void SetOrientation()
@@ -24,13 +46,14 @@ namespace CUDAFingerprinting.OrientationField
             double[,] filterX = new double[,] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
             double[,] filterY = new double[,] { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
 
-            Gx =  CUDAFingerprinting.Common.ConvolutionHelper.Convolve(pxl, filterX); //GradientX(pxl);
-			Gy =  CUDAFingerprinting.Common.ConvolutionHelper.Convolve(pxl, filterY); //GradientY(pxl)
+			// градиенты
+			double[,] Gx = CUDAFingerprinting.Common.ConvolutionHelper.Convolve(_pxl, filterX); 
+			double[,] Gy = CUDAFingerprinting.Common.ConvolutionHelper.Convolve(_pxl, filterY); 
             double numerator = 0;
             double denominator = 0;
-            for (int i = 0; i < SIZE; i++)
+            for (int i = 0; i < _size; i++)
             {
-                for (int j = 0; j < SIZE; j++)
+                for (int j = 0; j < _size; j++)
                 {
                     numerator += Gx[i, j] * Gy[i, j];
                     denominator += Gx[i, j] * Gx[i, j] - Gy[i, j] * Gy[i, j];
@@ -38,104 +61,57 @@ namespace CUDAFingerprinting.OrientationField
             }
             if (denominator == 0)
             {
-                this.orientation = Math.PI / 2;
+                this._orientation = Math.PI / 2;
             }
             else
             {
-            this.orientation = Math.Atan2(2 * numerator, denominator) / 2;
+            this._orientation = Math.Atan2(2 * numerator, denominator) / 2;
             }
         }
-
-		//public double[,] GradientX(double[,] bytes)           // возвращает массив градиентов по X после примененения маски Собеля
-		//{
-		//	int[,] filter = new int[,] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
-		//	double[,] result = new double[bytes.GetUpperBound(0) + 1, bytes.GetUpperBound(1) + 1];
-
-		//	for (int x = 0; x < bytes.GetUpperBound(0) + 1; x++)
-		//	{
-		//		for (int y = 0; y < bytes.GetUpperBound(1) + 1; y++)
-		//		{
-		//			for (int i = -1; i < 2; i++)
-		//			{
-		//				for (int j = -1; j < 2; j++)
-		//				{
-		//					// если не выходим за границы
-		//					if (x + i >= 0 && x + i <= bytes.GetUpperBound(0) &&
-		//						y + j >= 0 && y + j <= bytes.GetUpperBound(1))
-		//					{
-		//						result[x, y] += filter[i + 1, j + 1] * bytes[x + i, y + j];
-		//					}
-		//				}
-		//			}
-		//		}
-		//	}
-		//	return result;
-		//}
-
-		//public double[,] GradientY(double[,] bytes)           // возвращает массив градиентов по Y после примененения маски Собеля
-		//{
-		//	int[,] filter = new int[,] { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
-		//	double[,] result = new double[bytes.GetUpperBound(0) + 1, bytes.GetUpperBound(1) + 1];
-
-		//	for (int x = 0; x < bytes.GetUpperBound(0) + 1; x++)
-		//	{
-		//		for (int y = 0; y < bytes.GetUpperBound(1) + 1; y++)
-		//		{
-		//			for (int i = -1; i < 2; i++)
-		//			{
-		//				for (int j = -1; j < 2; j++)
-		//				{
-		//					// если не выходим за границы
-		//					if (x + i >= 0 && x + i <= bytes.GetUpperBound(0) &&
-		//						y + j >= 0 && y + j <= bytes.GetUpperBound(1))
-		//					{
-		//						result[x, y] += filter[i + 1, j + 1] * bytes[x + i, y + j];
-		//					}
-		//				}
-		//			}
-		//		}
-		//	}
-		//	return result;
-		//}
     }
-
-
 
     public class OrientationField
     {
-        public Block16[,] blocks;
-        public const int SIZE = 16;
+        Block[,] _blocks;
+        const int _SIZE = 16;
 
-        public OrientationField(byte[,] bytes)
+		// property
+		public Block[,] Blocks
+		{
+			get
+			{
+				return _blocks;
+			}
+		}
+		public int SIZE
+		{
+			get
+			{
+				return _SIZE;
+			}
+		}
+
+        public OrientationField(int[,] bytes)
         {
             int maxX = bytes.GetUpperBound(0) + 1;
             int maxY = bytes.GetUpperBound(1) + 1;
             // разделение на блоки
-            this.blocks = new Block16[(int)Math.Ceiling((float)(maxX / SIZE)), (int)Math.Ceiling((float)(maxY / SIZE))];
-            for (int i = 0; i < blocks.GetUpperBound(0) + 1; i++)
+            this._blocks = new Block[(int)Math.Ceiling((float)(maxX / _SIZE)), (int)Math.Ceiling((float)(maxY / _SIZE))];
+            for (int i = 0; i < _blocks.GetUpperBound(0) + 1; i++)
             {
-                for (int j = 0; j < blocks.GetUpperBound(1) + 1; j++)
+                for (int j = 0; j < _blocks.GetUpperBound(1) + 1; j++)
                 {
-                    blocks[i, j] = new Block16();
-                    // копируем в блоки части массива
-					for (int x = 0; x < SIZE; x++)
-					{
-						for (int y = 0; y < SIZE; y++)
-						{
-							blocks[i, j].pxl[x, y] = bytes[i * SIZE + x, j * SIZE + y];
-						}
-					}
-					// вычисляем направления
-                    blocks[i, j].SetOrientation();
+                    _blocks[i, j] = new Block(_SIZE, bytes, i * _SIZE, j * _SIZE);
                 }
             }
+
         }
 
 		public double GetOrientation(int x, int y)                  // метод, определяющий по входным координатам (х, у) поле напрваления в этой точке
 		{
-			int row = x / SIZE;
-			int column = y / SIZE;
-			return this.blocks[row, column].orientation;
+			int row = x / _SIZE;
+			int column = y / _SIZE;
+			return this._blocks[row, column].Orientation;
 		}
     } 
 }
