9ef23d487a28aed7f38d69ac87dd7a725e3b10da
SeriousCoder <0kintol0@gmail.com>
2015-07-02 12:51:07 +0300

    Added normalization function in CUDA

--- Code/CUDAFingerprinting.GPU.Common/Normalization.cu
+++ Code/CUDAFingerprinting.GPU.Common/Normalization.cu
@@ -1,7 +1,9 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdlib.h>
+#include <math.h>
 #include "CUDAArray.cuh"
+
 double CalculateMean(CUDAArray<float> image)
 {
 	int height = image.Height;
@@ -16,3 +18,25 @@ double CalculateMean(CUDAArray<float> image)
 	}
 	return mean;
 }
+
+CUDAArray<float> DoNormalization(CUDAArray<float> image, int bordMean, int bordVar)
+{
+	float mean = CalculateMean(image);
+	float variation = CalculateVariation(image, mean);
+
+	for (int i = 0; i < image.Width; i++)
+	{
+		for (int j = 0; j < image.Height; j++)
+		{
+			if (image.At(i, j) > mean)
+			{
+				image.SetAt(i, j, bordMean + sqrt((bordVar * pow(image.At(i, j) - mean, 2)) / variation));
+			}
+			else
+			{
+				image.SetAt(i, j), bordMean - sqrt((bordVar * pow(image.At(i, j) - mean, 2)) / variation));
+			}
+		}
+	}
+
+	return image;
