82d4faf2f0eddbb93de702ea1d28ceaa8db2c953
Alexander Chebykin <a.e.chebykin@gmail.com>
2015-07-02 11:36:42 +0300

    Methods to calculate mean and variation of an image added

--- Code/CUDAFingerprinting.Common/Normalization.cs
+++ Code/CUDAFingerprinting.Common/Normalization.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace CUDAFingerprinting.Common
 {
-    class Normalization
+    static class Normalization
     {
+        public static double CalculateMean (this double [,] image)
+        {
+            int height = image.GetLength (1);
+            int width  = image.GetLength (0);
+            double mean = 0;
+            for (int i = 0; i < height; i++) 
+            {
+                for (int j = 0; j < width; j++) 
+                {
+                    mean += image[i, j] / (height * width);
+                }
+            }
+            return mean;
+        }
+        public static double CalculateVariation(this double[,] image, double mean)
+        {
+            int height = image.GetLength(1);
+            int width  = image.GetLength(0);
+            double variation = 0;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    variation += Math.Pow ((image[i, j] - mean), 2) / (height * width);
+                }
+            }
+            return variation;
+        }
     }
 }
