1fd6dc431e2c47d4ee20b36e64e71a142738b8f5
annafatkina <annacplus@yandex.ru>
2015-07-02 14:52:13 +0300

    - Test for image binarialization added

---
+++ Code/CUDAFingerprinting.Common.ImageBinarialization.Test/ImageBinarializationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CUDAFingerprinting.Common.ImageBinarialization;
+using System.IO;
+
+namespace CUDAFingerprinting.Common.ImageBinarialization.Test
+{
+    [TestClass]
+    public class ImageBinarializationTest
+    {
+        [TestMethod]
+        public void BinarializationTest()
+        {
+            Bitmap img = Resources._2_6;
+            var resultImage = new ImageBinarialization(img);
+            resultImage.Binarizator(resultImage, 128).Save("ResultImage.bmp", ImageFormat.Bmp);
+        }
+    }
+}
