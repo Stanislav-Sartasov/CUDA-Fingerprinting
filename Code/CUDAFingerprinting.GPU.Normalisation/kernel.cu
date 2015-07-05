cf9b9475f2dfdcf24b45d9c8f35fe8212c60d6ed
Alexander Chebykin <a.e.chebykin@gmail.com>
2015-07-05 14:01:04 +0300

    Normalization works properly.

--- Code/CUDAFingerprinting.GPU.Normalisation/kernel.cu
+++ Code/CUDAFingerprinting.GPU.Normalisation/kernel.cu
@@ -1,21 +1,21 @@
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include "device_functions.h"
 #include <stdio.h>
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+#include "constsmacros.h"
 #include <stdlib.h>
 #include <math.h>
+
 #include "CUDAArray.cuh"
+
 __global__ void cudaCalcMeanRow(CUDAArray<float> source, float* meanArray)
 {
-
-	int row = defaultRow();
+	
+	int row = defaultColumn();
 	int height = source.Height;
 	int width = source.Width;
 	int tempIndex = threadIdx.x;
-
+	
 	__shared__ float* temp;
 	temp = (float*)malloc(blockDim.x * sizeof(float));
 	float mean = 0;
@@ -23,35 +23,40 @@ __global__ void cudaCalcMeanRow(CUDAArray<float> source, float* meanArray)
 	{
 		for (int j = 0; j < source.Width; j++)
 		{
-			mean += source.At(row, j) / (height * width);
+			//mean += source.At(row, j) / (height * width);
+			mean += source.cudaPtr[row*(source.Stride / sizeof(float)) + j] / (height * width);
 		}
 	}
+	
 	temp[tempIndex] = mean;
 	__syncthreads();
-
+	
 	//This is reduction.It will work only if number of threads in the block is a power of 2.
 	int i = blockDim.x / 2;
 	while (i != 0)
 	{
-		if (tempIndex < i)
+		if (tempIndex < i) 
 			temp[tempIndex] += temp[tempIndex + i];
 		__syncthreads();
 		i /= 2;
 	}
-	if (tempIndex == 0)
-		meanArray = temp;//we need to write it only one time. Why not to choose the first thread for this purpose?
+	if (tempIndex == 0) 
+		meanArray[blockIdx.x] = temp[0];//we need to write it only one time. Why not to choose the first thread for this purpose?
+		
 }
+
 
 float CalculateMean(CUDAArray<float> image)
 {
 	int height = image.Height;
 	float *dev_mean, mean = 0;
-
+	
 	dim3 blockSize = dim3(defaultThreadCount);
 	dim3 gridSize = dim3(ceilMod(height, defaultThreadCount));
 	float* meanArray = (float*)malloc(gridSize.x * sizeof(float));
+	cudaMalloc((void**)&dev_mean, gridSize.x * sizeof(float));
 
-	cudaCalcMeanRow << <gridSize, blockSize >> > (image, dev_mean);
+	cudaCalcMeanRow <<<gridSize, blockSize >>> (image, dev_mean);
 	cudaMemcpy(meanArray, dev_mean, gridSize.x * sizeof(float), cudaMemcpyDeviceToHost);
 	for (int i = 0; i < height; i++)
 	{
@@ -63,7 +68,7 @@ float CalculateMean(CUDAArray<float> image)
 __global__ void cudaCalcVariationRow(CUDAArray<float> image, float mean, float* variationArray)
 {
 
-	int row = defaultRow();
+	int row = defaultColumn();
 	int height = image.Height;
 	int width = image.Width;
 	int tempIndex = threadIdx.x;
@@ -75,7 +80,7 @@ __global__ void cudaCalcVariationRow(CUDAArray<float> image, float mean, float* 
 	{
 		for (int j = 0; j < image.Width; j++)
 		{
-			variation += pow((image.At(row, j) - mean), 2) / (height * width);
+			variation += pow((image.cudaPtr[row*(image.Stride / sizeof(float)) + j] - mean), 2) / (height * width);
 		}
 	}
 	temp[tempIndex] = variation;
@@ -90,7 +95,7 @@ __global__ void cudaCalcVariationRow(CUDAArray<float> image, float mean, float* 
 		i /= 2;
 	}
 	if (tempIndex == 0)
-		variationArray = temp;//we need to write it only one time. Why not to choose the first thread for this purpose?
+		variationArray[blockIdx.x] = temp[0];//we need to write it only one time. Why not to choose the first thread for this purpose?
 }
 
 float CalculateVariation(CUDAArray<float> image, float mean)
@@ -101,8 +106,9 @@ float CalculateVariation(CUDAArray<float> image, float mean)
 	dim3 blockSize = dim3(defaultThreadCount);
 	dim3 gridSize = dim3(ceilMod(height, defaultThreadCount));
 	float* variationArray = (float*)malloc(gridSize.x * sizeof(float));
+	cudaMalloc((void**)&dev_variation, gridSize.x * sizeof(float));
 
-	cudaCalcVariationRow << <gridSize, blockSize >> > (image, mean, dev_variation);
+	cudaCalcVariationRow <<<gridSize, blockSize >>> (image, mean, dev_variation);
 	cudaMemcpy(variationArray, dev_variation, gridSize.x * sizeof(float), cudaMemcpyDeviceToHost);
 	for (int i = 0; i < height; i++)
 	{
@@ -112,19 +118,23 @@ float CalculateVariation(CUDAArray<float> image, float mean)
 }
 __global__ void cudaDoNormalizationRow(CUDAArray<float> image, float mean, float variation, int bordMean, int bordVar)
 {
-	int row = defaultRow();
+	int row = defaultColumn();
 
 	if (image.Height > row)
 	{
 		for (int j = 0; j < image.Width; j++)
 		{
-			if (image.At(row, j) > mean)
+			//if (image.At(row, j) > mean)
+			float at = image.cudaPtr[row*(image.Stride / sizeof(float)) + j];
+			if (image.cudaPtr[row*(image.Stride / sizeof(float)) + j] > mean)
 			{
-				image.SetAt(row, j, bordMean + sqrt((bordVar * pow(image.At(row, j) - mean, 2)) / variation));
+				//image.SetAt(row, j, bordMean + sqrt((bordVar * pow(image.At(row, j) - mean, 2)) / variation));
+				at = bordMean + sqrt((bordVar * pow(at - mean, 2)) / variation);
 			}
 			else
 			{
-				image.SetAt(row, j, bordMean - sqrt((bordVar * pow(image.At(row, j) - mean, 2)) / variation));
+				//image.SetAt(row, j, bordMean - sqrt((bordVar * pow(image.At(row, j) - mean, 2)) / variation));
+				at = bordMean - sqrt((bordVar * pow(at - mean, 2)) / variation);
 			}
 		}
 	}
@@ -139,165 +149,10 @@ CUDAArray<float> DoNormalization(CUDAArray<float> image, int bordMean, int bordV
 
 	dim3 blockSize = dim3(defaultThreadCount);
 	dim3 gridSize = dim3(ceilMod(height, defaultThreadCount));
-	cudaDoNormalizationRow << <gridSize, blockSize >> > (image, mean, variation, bordMean, bordVar);
+	cudaDoNormalizationRow <<<gridSize, blockSize >>> (image, mean, variation, bordMean, bordVar);
 	return image;
 }
+
 void main()
 {
 }
-/*
-
-CUDAArray<float> DoNormalization(CUDAArray<float> image, int bordMean, int bordVar)
-{
-float mean = CalculateMean(image);
-float variation = CalculateVariation(image, mean);
-
-for (int i = 0; i < image.Width; i++)
-{
-for (int j = 0; j < image.Height; j++)
-{
-if (image.At(i, j) > mean)
-{
-image.SetAt(i, j, bordMean + sqrt((bordVar * pow(image.At(i, j) - mean, 2)) / variation));
-}
-else
-{
-image.SetAt(i, j), bordMean - sqrt((bordVar * pow(image.At(i, j) - mean, 2)) / variation));
-}
-}
-}
-
-return image;
-float CalculateVariation(CUDAArray<float> image, float mean)
-{
-int height = image.Height;
-int width = image.Width;
-float variation = 0;
-for (int i = 0; i < width; i++)
-{
-for (int j = 0; j < height; j++)
-{
-variation += pow((image.At(i, j) - mean), 2) / (height * width);
-}
-}
-return variation;
-}
-
-*/
-/*
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
-
-int main()
-{
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-    return 0;
-}
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
-*/
