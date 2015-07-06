f271a7f5ddfd668749c094555cffcc8a44ae4412
annafatkina <annacplus@yandex.ru>
2015-07-06 10:02:48 +0300

    - Test for ImageBinarization with images as int and double arrays.

---
+++ Code/CUDAFingerprinting.Common.ImageBinarization.Test/ImageBinarizationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CUDAFingerprinting.Common.ImageBinarization;
+using System.IO;
+
+namespace CUDAFingerprinting.Common.ImageBinarization.Test
+{
+    [TestClass]
+    public class ImageBinarizationTest
+    {
+        [TestMethod]
+        public void BinarializationTest()
+        {
+            double[,] arrayD = ImageHelper.LoadImage(Resources._2_6);
+            var binarizatedImageDouble = ImageBinarization.Binarizator2D(arrayD, 128);
+            ImageHelper.SaveArrayToBitmap(binarizatedImageDouble).Save(Path.GetTempPath()+ Guid.NewGuid() + ".bmp");
+ 
+            int[,] arrayI = ImageHelper.LoadImageAsInt(Resources._2_6);
+            var binarizatedImageInt = ImageBinarization.Binarizator2D(arrayI, 128);
+            ImageHelper.SaveArrayToBitmap(binarizatedImageInt).Save(Path.GetTempPath() + Guid.NewGuid() + ".bmp");
+        }
+    }
+}
