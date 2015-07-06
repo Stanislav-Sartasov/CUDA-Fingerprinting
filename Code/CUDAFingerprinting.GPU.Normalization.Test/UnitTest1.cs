7800019daaf750a68feb527db98e3dd063cf08b0
Tarasenko Nikita <nik.d.tarasenko@gmail.com>
2015-07-06 15:00:19 +0300

    Created test, but it don't want running

--- Code/CUDAFingerprinting.GPU.Normalization.Test/UnitTest1.cs
+++ Code/CUDAFingerprinting.GPU.Normalization.Test/UnitTest1.cs
@@ -4,33 +4,63 @@ using System.IO;
 using System.Text;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using CUDAFingerprinting.Common;
 
-namespace CUDA
-{
-    public class NormalizeClass
-    {
-        [DllImport("CUDAFingerprinting.GPU.Normalisation.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Normalize")]
-        public static extern float[,] Normalize(float[,] source, int imgWidth, int imgHeight, int bordMean, int bordVar);
-    }
-}
 
 namespace CUDAFingerprinting.GPU.Normalization.Test
 {
     [TestClass]
     public class UnitTest1
     {
-        
+        public static float[,] LoadImage(Bitmap bmp)
+        {
+            float[,] imgBytes = new float[bmp.Height, bmp.Width];
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    imgBytes[bmp.Height - 1 - y, x] = bmp.GetPixel(x, y).R;
+                }
+            }
+            return imgBytes;
+        }
+
+        public static Bitmap SaveArrayToBitmap(float[,] data)
+        {
+            int x = data.GetLength(1);
+            int y = data.GetLength(0);
+            var max = float.NegativeInfinity;
+            var min = float.PositiveInfinity;
+            foreach (var num in data)
+            {
+                if (num > max) max = num;
+                if (num < min) min = num;
+            }
+            var bmp = new Bitmap(x, y);
+            data.Select2D((value, row, column) =>
+            {
+                var gray = (int)((value - min) / (max - min) * 255);
+                lock (bmp)
+                    bmp.SetPixel(column, bmp.Height - 1 - row, Color.FromArgb(gray, gray, gray));
+                return value;
+            });
+            return bmp;
+        }
+
+        [DllImport("CUDAFingerprinting.GPU.Normalisation.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Normalize")]
+        public static extern float[,] Normalize(float[,] source, int imgWidth, int imgHeight, int bordMean, int bordVar);
+
         [TestMethod]
         public void NormalizationTest()
         {
-            var bmp = Resources.SampleFinger2;
-            var array = ImageLoading.LoadBmp(bmp);
+            var bmp = Resources.SimpleFinger1;
+            var array = LoadImage(bmp);
 
-            array = array.DoNormalization(1000, 1000);
+            array = Normalize(array, bmp.Width, bmp.Height, 1000, 1000);
 
-            var bmp2 = ImageHelper.SaveArrayToBitmap(array);
+            var bmp2 = SaveArrayToBitmap(array);
 
-            bmp2.Save("test.bmp", ImageHelper.GetImageFormatFromExtension("test.bmp"));
+            bmp.Save("test.bmp", ImageHelper.GetImageFormatFromExtension("test.bmp"));
         }
     }
 }
