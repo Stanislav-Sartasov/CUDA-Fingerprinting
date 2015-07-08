b288e97909feda5e4d5e0461662b4a2511607ccd
annafatkina <annacplus@yandex.ru>
2015-07-08 10:06:10 +0300

    - Test for ImageBinarization with asserts

--- Code/CUDAFingerprinting.Common.ImageBinarization.Test/ImageBinarizationTest.cs
+++ Code/CUDAFingerprinting.Common.ImageBinarization.Test/ImageBinarizationTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Drawing;
 using System.Drawing.Imaging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -15,10 +16,44 @@ namespace CUDAFingerprinting.Common.ImageBinarization.Test
         {
             double[,] arrayD = ImageHelper.LoadImage(Resources._2_6);
             var binarizatedImageDouble = ImageBinarization.Binarizator2D(arrayD, 128);
+            for (int i = 0; i < arrayD.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayD.GetLength(1); j++)
+                {
+                    if (arrayD[i, j] < 128)
+                    {
+                        arrayD[i, j] = 0;
+                    }
+                    else
+                    {
+                        arrayD[i, j] = 255;
+                    }
+
+                    // Check binarizatedImageDouble
+                    Assert.AreEqual(arrayD[i, j], binarizatedImageDouble[i, j]);
+                }
+            }
             ImageHelper.SaveArrayToBitmap(binarizatedImageDouble).Save(Path.GetTempPath()+ Guid.NewGuid() + ".bmp");
  
             int[,] arrayI = ImageHelper.LoadImageAsInt(Resources._2_6);
             var binarizatedImageInt = ImageBinarization.Binarizator2D(arrayI, 128);
+            for (int i = 0; i < arrayI.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayI.GetLength(1); j++)
+                {
+                    if (arrayD[i, j] < 128)
+                    {
+                        arrayD[i, j] = 0;
+                    }
+                    else
+                    {
+                        arrayD[i, j] = 255;
+                    }
+
+                    // Check binarizatedImageDouble
+                    Assert.AreEqual(arrayD[i, j], binarizatedImageDouble[i, j]);
+                }
+            }
             ImageHelper.SaveArrayToBitmap(binarizatedImageInt).Save(Path.GetTempPath() + Guid.NewGuid() + ".bmp");
         }
     }
