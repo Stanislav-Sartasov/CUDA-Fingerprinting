5d4320b003789655e6b91f6b664270fb918ee0ca
Alexander Chebykin <a.e.chebykin@gmail.com>
2015-07-09 22:26:12 +0300

    ImageEnhancment.cs updated and moved to proper project.

--- Code/CUDAFingerprinting.Common.GaborFilter/ImageEnhancement.cs
+++ Code/CUDAFingerprinting.Common.GaborFilter/ImageEnhancement.cs
@@ -1,17 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-
-namespace CUDAFingerprinting.Common
+using CUDAFingerprinting.Common.GaborFilter;
+namespace CUDAFingerprinting.Common.GaborFilter
 {
     class ImageEnhancement
     {
         public static double[,] Enhance(double[,] img, double[,] orientMatrix, double frequency, int filterSize,
-            float[] angles)
+            int angleNum)
         {
             double[,] result = new double[img.GetLength(0),img.GetLength(1)];
+            double [] angles = new double[angleNum];
+            double constAngle = Math.PI/angleNum;
+            for (int i = 0; i < angleNum; i++)
+                angles[i] = constAngle*i;
+            var gabor = new GaborFilter(angleNum, filterSize);
             for (int i = 0; i < img.GetLength(0); i++)
                 for (int j = 0; j < img.GetLength(1); j++)
                 {
@@ -20,14 +21,14 @@ namespace CUDAFingerprinting.Common
                         for (int v = -filterSize/2; v <= filterSize/2; v++)
                         {
                             double diff = Double.MaxValue;
-                            double angle = 0;
-                            for (int angInd = 0; angInd < angles.Length; angInd++)
+                            int angle = 0;
+                            for (int angInd = 0; angInd < angleNum; angInd++)
                                 if (Math.Abs(angles[angInd] - img[i, j]) < diff)
                                 {
-                                    angle = angles[angInd];
+                                    angle = angInd;
                                     diff = Math.Abs(angles[angInd] - img[i, j]);
                                 }
-                            enhancedPxl += Gabor(u, v, angle, frequency) * img[i - u, j - v];//Insert real Gabor filtering function name.
+                            enhancedPxl += gabor.Filters[angle].Matrix[u, v] * img[i - u, j - v];
                         }
                     result[i, j] = enhancedPxl;
                 }
