7497eeccda388383d3c8e0f78c150932feeda25d
SeriousCoder <0kintol0@gmail.com>
2015-07-09 13:47:44 +0300

    Added Gabor filter.

---
+++ Code/CUDAFingerprinting.Common.GaborFilter/GaborFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUDAFingerprinting.Common.GaborFilter
+{
+    class GaborFilter
+    {
+        public Filter[] Filters;
+        public int Count;
+
+        public GaborFilter(int count, int size)
+        {
+            var bAngle = Math.PI / count;
+            Filters = new Filter[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Filters[i] = new Filter(size, bAngle * i);
+            }
+        }
+    }
+}
