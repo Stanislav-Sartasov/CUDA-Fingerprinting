2cc34100541e97c83949fa48fb18081973f2b072
annafatkina <annacplus@yandex.ru>
2015-07-09 11:22:31 +0300

    - Some variables were renamed, array for result image was added (ImageBinarization/kernel.cu).

--- Code/CUDAFingerprinting.GPU.ImageBinarization.CUDATest/ImageBinarizationTest.cs
+++ Code/CUDAFingerprinting.GPU.ImageBinarization.CUDATest/ImageBinarizationTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Runtime.InteropServices;
-using  CUDAFingerprinting.Common;
+using CUDAFingerprinting.Common;
 using System.IO;
 
 namespace CUDAFingerprinting.GPU.ImageBinarization.CUDATest
@@ -10,16 +10,16 @@ namespace CUDAFingerprinting.GPU.ImageBinarization.CUDATest
     public class ImageBinarizationTest
     {
         [DllImport("CUDAFingerprinting.GPU.ImageBinarization.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "BinarizateImage")]
-        public static extern void BinarizateImage(int line, int[,] dist, int width, int height);
+        public static extern void BinarizateImage(int line, int[,] src, int[,] dist, int width, int height);
         [TestMethod]
         public void BinarizationTest()
         {
             int[,] image = ImageHelper.LoadImageAsInt(Resources._2_6);
-            int x = image.GetLength(0);
-            int y = image.GetLength(1);
-            BinarizateImage(128, image, x, y);
-            ImageHelper.SaveArrayToBitmap(image).Save(Path.GetTempPath() + Guid.NewGuid() + ".bmp");
-
+            int width = image.GetLength(0);
+            int height = image.GetLength(1);
+            int[,] dist = new int[width, height];
+            BinarizateImage(128, image, dist, width, height);
+            ImageHelper.SaveArrayToBitmap(dist).Save(Path.GetTempPath() + Guid.NewGuid() + ".bmp");
         }
     }
 }
