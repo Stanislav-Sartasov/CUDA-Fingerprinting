d1d5f3a3d943f8e9dcaf391806becee28f78f235
Tarasenko Nikita <nik.d.tarasenko@gmail.com>
2015-07-10 16:01:44 +0300

    Now Fingerprinting.GPU builds successfully

--- Code/CUDAFingerprinting.GPU.Filters/Filter.cu
+++ Code/CUDAFingerprinting.GPU.Filters/Filter.cu
@@ -1,2 +1,26 @@
+#include "CUDAArray.cuh"
+#include <stdlib.h>
 
+#define _USE_MATH_DEFINES
+#include <math.h>
 
+CUDAArray<float> Filter(int size, float angle, float frequency)
+{
+	float* matrix = (float*)malloc(size * size * sizeof(float));
+
+	float aCos = cos(M_PI / 2 + angle);
+	float aSin = sin(M_PI / 2 + angle);
+
+	int center = size / 2;
+	int upperCenter = (size & 1) == 1 ? center - 1 : center;
+
+	for (int i = -upperCenter; i < center; i++)
+	{
+		for (int j = -upperCenter; j < center; j++)
+		{
+			matrix[i * size + j] = exp(-0.5 * ((i * aSin + j * aCos) * (i * aSin + j * aCos) / 16 + (-i *aCos + j * aSin) * (-i *aCos + j * aSin) / 16)) * cos(2 * M_PI * (i * aSin + j * aCos) * frequency);
+		}
+	}
+
+	return CUDAArray<float>(matrix, size, size);
+}
