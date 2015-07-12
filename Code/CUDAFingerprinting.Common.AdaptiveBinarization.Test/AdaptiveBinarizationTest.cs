5b31e2b9d8211a60fa3fcc639e1de4292f8b2836
annafatkina <annacplus@yandex.ru>
2015-07-13 01:35:09 +0300

    - ProjectionX was changed (another way to compute coordinates of point). Test for AdaptiveBinarization was added (doesn't work).

---
+++ Code/CUDAFingerprinting.Common.AdaptiveBinarization.Test/AdaptiveBinarizationTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using CUDAFingerprinting.Common.OrientationField;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CUDAFingerprinting.Common.AdaptiveBinarization.Test
+{
+    [TestClass]
+    public class AdaptiveBinarizationTest
+    {
+        [TestMethod]
+        public void AdaptiveBinarizationTestMethod()
+        {
+            int[,] arrayI = ImageHelper.LoadImageAsInt(Resources.test);
+            
+            OrientationField.OrientationField field = new OrientationField.OrientationField(arrayI);
+
+            field.SaveAboveToFile(Resources.test, Path.GetTempPath() + Guid.NewGuid() + ".bmp", true);
+            
+            var binarizatedInt = AdaptiveBinarization.AdaptiveImageBinarization(arrayI);
+            ImageHelper.SaveArrayToBitmap(binarizatedInt).Save(Path.GetTempPath() + Guid.NewGuid() + ".bmp");
+        }
+    }
+}
--- Code/CUDAFingerprinting.Common.AdaptiveBinarization/AdaptiveBinarization.cs
+++ Code/CUDAFingerprinting.Common.AdaptiveBinarization/AdaptiveBinarization.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using CUDAFingerprinting.Common.OrientationField;
 
 namespace CUDAFingerprinting.Common.AdaptiveBinarization
 {
@@ -10,13 +12,14 @@ namespace CUDAFingerprinting.Common.AdaptiveBinarization
 
     public class AdaptiveBinarization
     {
-        public const int FieldSize = 16;
+        public const int FieldSizex = 16;
+        
 
         public static Point Turn(int x, int y, int xCentre, int yCentre, double angleCos, double angleSin)
         {
             Point p;
-            p.X = (int) Math.Round(xCentre + (x - xCentre)*angleCos + (y - yCentre)*angleSin);
-            p.Y = (int)Math.Round(yCentre - (x - xCentre) * angleSin + (y - yCentre) *angleCos);
+            p.X = (int) Math.Floor(xCentre + (x - xCentre)*angleCos - (y - yCentre)*angleSin);
+            p.Y = (int)Math.Floor(yCentre + (x - xCentre) * angleSin + (y - yCentre) *angleCos);
             return p;
         }
 
@@ -33,28 +36,46 @@ namespace CUDAFingerprinting.Common.AdaptiveBinarization
 
         public static int[] ProjectionX(int xCentre, int yCenre, int[,] arr) //  orthogonal projection on Ox
         {
+            int FieldSizey = 8;
             // просуммировать по игреку все в этом ряду из блока 16х16 ортоганально оси Ох
-            int[] projX = new int[FieldSize]; //16
+            int[] projX = new int[FieldSizex]; //16
             OrientationField.OrientationField img = new OrientationField.OrientationField(arr); // size of bmp
+            
+
+
             var angleOfX = img.GetOrientation(xCentre, yCenre) - Math.PI/2.0;
 
             double angleSin, angleCos;
-            double localSegment;
+           // double localSegment;
             Point tmpPoint;
-
-            for (int i = -FieldSize/2; i < FieldSize/2; i++)
+            angleSin = Math.Sin(angleOfX);
+            angleCos = Math.Cos(angleOfX);
+            for (int i = -FieldSizex/2; i < FieldSizex/2; i++)
             {
-                projX[i + FieldSize/2] = 0;
-                for (int j = -FieldSize/2; j < FieldSize/2; j++) //summ all column elements
+                projX[i + FieldSizex/2] = 0;
+                for (int j = -FieldSizey/2; j < FieldSizey/2; j++) //summ all column elements
                 {
-                    localSegment = Math.Sqrt(i*i + j*j);
-                    if (Math.Abs(localSegment) < 0.000001) continue; // double tolerance
+                   // localSegment = Math.Sqrt(i*i + j*j);
                     
-                    angleSin = Math.Sin(angleOfX + Math.Asin(find_sin(1.0, 0.0, i, j)));
-                    angleCos = Math.Cos(angleOfX + Math.Acos(find_cos(1.0, 0.0, i, j)));
-                    tmpPoint = Turn(0, (int) Math.Floor(localSegment), 0, 0, angleCos, angleSin);
-                    projX[i + FieldSize/2] += arr[tmpPoint.X + xCentre, tmpPoint.Y + yCenre]/FieldSize;
+                    if (i == 0 && j == 0) continue; // double tolerance
+                    
+                    //angleSin = Math.Sin(angleOfX + Math.Asin(find_sin(1.0, 0.0, i, j)));
+                    //angleCos = Math.Cos(angleOfX + Math.Acos(find_cos(1.0, 0.0, i, j)));
+                    //tmpPoint = Turn(0, (int) Math.Floor(localSegment), 0, 0, angleCos, angleSin);
+                    tmpPoint = Turn(i, j, 0, 0, angleCos, angleSin);
+                    if (tmpPoint.X + xCentre < 0 || tmpPoint.X + xCentre >= arr.GetLength(0) || tmpPoint.Y + yCenre < 0 ||
+                        tmpPoint.Y + yCenre >= arr.GetLength(1))
+                    {
+                        FieldSizey--;
+                        continue;
+                    }
+                    projX[i + FieldSizex/2] += arr[tmpPoint.X + xCentre, tmpPoint.Y + yCenre];
                 }
+                if (FieldSizey <= 0)
+                {
+                    continue;
+                }
+                projX[i + FieldSizex/2] /= FieldSizey;
             }
             return projX;
         }
@@ -62,26 +83,26 @@ namespace CUDAFingerprinting.Common.AdaptiveBinarization
         public static int[,] AdaptiveImageBinarization(int[,] arr)
         {
             int[,] resArr = new int[arr.GetLength(0),arr.GetLength(1)]; // values 0 or 255
-
-            for (int xCentre = FieldSize; xCentre < arr.GetLength(0)-FieldSize; xCentre++) //смени границы
+            
+            for (int xCentre = FieldSizex; xCentre < arr.GetLength(0)-FieldSizex; xCentre++) //смени границы
             {
-                for (int yCente = FieldSize; yCente < arr.GetLength(1)-FieldSize; yCente++)
+                for (int yCente = FieldSizex; yCente < arr.GetLength(1)-FieldSizex; yCente++)
                 {
                     var projX = ProjectionX(xCentre, yCente, arr);
                     double max = 0;
-                    for (int k = -FieldSize / 2; k < FieldSize / 2 ; k++)
+                    for (int k = -FieldSizex / 2; k < FieldSizex / 2 ; k++)
                     {
                         if (k == 0)
                         {
                             continue;
                         }
-                        if (max < projX[k+FieldSize/2])
+                        if (max < projX[k+FieldSizex/2])
                         {
-                            max = projX[k+FieldSize/2];
+                            max = projX[k+FieldSizex/2];
                         }
                     }
 
-                    if (projX[FieldSize / 2] >= max)
+                    if (projX[FieldSizex / 2] >= max)
                     {
                         resArr[xCentre, yCente] = 255;
                     }
@@ -95,7 +116,7 @@ namespace CUDAFingerprinting.Common.AdaptiveBinarization
             return resArr;
         }
             
-
+        
 
     }
 }
