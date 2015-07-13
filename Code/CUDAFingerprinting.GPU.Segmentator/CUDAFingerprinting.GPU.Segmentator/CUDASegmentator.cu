8ed8fa94ba0b3b340c8903f02a892ae01815ddec
klimovefim <klimov.efim@mail.ru>
2015-07-13 15:28:50 +0300

    - Convolve function replaced by simple CPU Sobel Operator (were some troubles with using GPU Sobel Operator). - Added recognition for each pixel in "right" blocks.

--- Code/CUDAFingerprinting.GPU.Segmentator/CUDAFingerprinting.GPU.Segmentator/CUDASegmentator.cu
+++ Code/CUDAFingerprinting.GPU.Segmentator/CUDAFingerprinting.GPU.Segmentator/CUDASegmentator.cu
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-#define edge 100
+#define edge 80
 #define defaultBlockSize 16
 
 extern "C"
@@ -21,7 +21,7 @@ extern "C"
 	__declspec( dllexport ) __global__ void cudaSegmentate (CUDAArray<float> value, int* matrix);
 }
 
-__global__ void cudaCalculate(CUDAArray<float> value, CUDAArray<float> Gx, CUDAArray<float> Gy)
+/*__global__ void cudaCalculate(CUDAArray<float> value, CUDAArray<float> Gx, CUDAArray<float> Gy)
 {
 	int row = defaultRow();
 	int column = defaultColumn();
@@ -45,54 +45,83 @@ void Calculate(CUDAArray<float> value, CUDAArray<float> Gx, CUDAArray<float> Gy)
 	dim3 gridSize = dim3(ceilMod(value.Width, defaultBlockSize), ceilMod(value.Height, defaultBlockSize));
 
 	cudaCalculate<<< gridSize, blockSize >>>(value, Gx, Gy);
-}
+}*/
+
 
 CUDAArray<float> SobelFilter (CUDAArray<float> source, int picWidth, int picHeight)
 {
 	float filterXLinear[9] = { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
 	float filterYLinear[9] = { -1, -2, -1, 0, 0, 0, 1, 2, 1 };
 
-	CUDAArray<float> filterX(filterXLinear, 3, 3);
-	CUDAArray<float> filterY(filterYLinear, 3, 3);
-	
-	CUDAArray<float> Gx(picWidth, picHeight);
-	CUDAArray<float> Gy(picWidth, picHeight);
+	CUDAArray<float> cudaFilterX(filterXLinear, 3, 3);
+	CUDAArray<float> cudaFilterY(filterYLinear, 3, 3);
 
-	Convolve(Gx, source, filterX);
-	Convolve(Gy, source, filterY);
+	float* filterX = (float*) malloc (sizeof (float)*3*3);
+	cudaFilterX.GetData (filterX);
 
-	CUDAArray<float> value = CUDAArray<float>(picWidth, picHeight);
-	Calculate(value, Gx, Gy); // Calculates result of Sobel Operator
+	float* filterY = (float*) malloc (sizeof (float)*3*3);
+	cudaFilterY.GetData (filterY);
 
-	filterX.Dispose();
-	filterY.Dispose();
-	Gx.Dispose();
-	Gy.Dispose();
+	float* fSource = (float*) malloc (sizeof (float)*picWidth*picHeight);
+	source.GetData (fSource);
+	float* fRes = (float*) malloc (sizeof (float)*picWidth*picHeight);
+
+	for (int x = 1; x < picWidth - 1; x++)
+    {
+        for (int y = 1; y < picHeight - 1; y++)
+        {
+            float sumX = fSource[x - 1 + picWidth * (y - 1)] * filterX[0] + fSource[x + 1 + picWidth * (y - 1)] * filterX[2] +
+                            fSource[x - 1 + picWidth * y] * filterX[3] + fSource[x + 1 + picWidth * y] * filterX[5] +
+                            fSource[x - 1 + picWidth * (y + 1)] * filterX[6] + fSource[x + 1 + picWidth * (y + 1)] * filterX[8];
+            float sumY = fSource[x - 1 + picWidth * (y - 1)] * filterY[0] + fSource[x + picWidth * (y - 1)] * filterY[1] + fSource[x + 1 + picWidth * (y - 1)] * filterY[2] +
+                fSource[x - 1 + picWidth * (y + 1)] * filterY[6] + fSource[x + picWidth * (y + 1)] * filterY[7] + fSource[x + 1 + picWidth * (y + 1)] * filterY[8];
+            double sqrtXY = sqrt(sumX * sumX + sumY * sumY);
+
+            sqrtXY = sqrtXY > 255 ? 255 : sqrtXY;
+
+			fRes [x + picWidth * y] = sqrtXY;
+        }
+    }
+
+	CUDAArray<float> value = CUDAArray<float>(fRes, picWidth, picHeight);
+
+	/*int* SOPic = (int*)malloc(sizeof(int)*value.Width*value.Height);
+	for ( int i = 0; i < picWidth*picHeight; i++ )
+	{
+		SOPic[i] = (int) fRes[i];
+	}
+	saveBmp ("SOPic.bmp", SOPic, picWidth, picHeight);*/
+
+	cudaFilterX.Dispose();
+	cudaFilterY.Dispose();
 
 	return value;
 }
 
 __global__ void cudaMatrix (CUDAArray<float> value, CUDAArray<int> matrix2D)
 {
-	int row = defaultRow ();
-	int column = defaultColumn ();
+	int row = blockIdx.y * defaultBlockSize + threadIdx.y;
+	int column = blockIdx.x * defaultBlockSize + threadIdx.x;
 
-	__shared__ float buf[16][16];
-	buf[threadIdx.x][threadIdx.y] = value.At (row, column);
+	int tX = threadIdx.x;
+	int tY = threadIdx.y;
+
+	__shared__ float buf[defaultBlockSize][defaultBlockSize];
+	buf[tX][tY] = value.At (row, column);
 
 	__syncthreads();
-	if ( threadIdx.x == 0 )
+	if ( tX == 0 )
 	{
 		float sum = 0;
 		for ( int i = 0; i < defaultBlockSize; ++i )
 		{
-			sum += buf[i][threadIdx.y];
+			sum += buf[i][tY];
 		}
-		buf[0][threadIdx.y] = sum;
+		buf[0][tY] = sum;
 	}
 
 	__syncthreads();
-	if ( threadIdx.x == 0 && threadIdx.y == 0 )
+	if ( tX == 0 && tY == 0 )
 	{
 		float sum = 0;
 		for ( int i = 0; i < defaultBlockSize; ++i )
@@ -107,7 +136,10 @@ __global__ void cudaMatrix (CUDAArray<float> value, CUDAArray<int> matrix2D)
 
 	if ( val >= edge )
 	{
-		matrix2D.SetAt (row, column, 1);
+		if ( buf[tX][tY] >= edge )
+			matrix2D.SetAt (row, column, 1);
+		else
+			matrix2D.SetAt (row, column, 0);
 	}
 	else
 	{
@@ -161,12 +193,23 @@ void Segmentate (CUDAArray<float> value, int* matrix)
 
 	matrix2D.GetData(matrix);
 
+	ofstream f;
+	f.open ("matrix.txt");
+	for ( int i = 0; i < value.Width; ++i )
+	{
+		for ( int j = 0; j < value.Height; ++j )
+		{
+			f << matrix[i * value.Width + j] << ' ';
+		}
+		f << endl;
+	}
+
 	matrix2D.Dispose();
 }
 
 void BWPicture (int width, int height, int* matrix)
 {
-	int* newPic;
+	int* newPic = (int*) malloc (sizeof (int)*width*height);;
 	int capacity = width * height;
 
 	for ( int i = 0; i < capacity; ++i )
@@ -177,7 +220,7 @@ void BWPicture (int width, int height, int* matrix)
 	saveBmp ("newPic.bmp", newPic, width, height);
 }
 
-void MakingMatrix (float* fPic, int picWidth, int picHeight, int* &matrix)
+void MakingMatrix (float* fPic, int picWidth, int picHeight, int* matrix)
 {
 	CUDAArray<float> source = CUDAArray<float>(fPic, picWidth, picHeight);
 
@@ -195,8 +238,8 @@ int main()
 	cudaSetDevice (0);
 
 	int picWidth, picHeight;
-	int* pic = loadBmp ("1_1.bmp", &picWidth, &picHeight);
-	float* fPic = (float*)malloc(sizeof(float)*picWidth*picHeight);
+	int* pic = loadBmp ("..//1_1.bmp", &picWidth, &picHeight);
+	float* fPic  = (float*) malloc (sizeof (float)*picWidth*picHeight);
 	for ( int i = 0; i < picWidth*picHeight; i++ )
 	{
 		fPic[i] = (float) pic[i];
