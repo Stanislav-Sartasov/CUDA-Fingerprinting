e60ee2857164295c9735a0379e5cf76930a0947f
Tarasenko Nikita <nik.d.tarasenko@gmail.com>
2015-07-13 14:41:47 +0300

    A little changes in Gabor Filter. Unittests don't work

--- Code/CUDAFingerprinting.GPU.Filters/Filter.cu
+++ Code/CUDAFingerprinting.GPU.Filters/Filter.cu
@@ -6,42 +6,76 @@
 #include <stdlib.h>
 #include "CUDAArray.cuh"
 #include <float.h>
-#define _USE_MATH_DEFINES
-#include <math.h>
+#include "math_constants.h"
+#include "math_functions.h"
 #include "CUDAArray.cuh"
 
 extern "C"
 {
-	__declspec(dllexport) void MakeGaborFilters(float* filter, int size, int angleNum, float frequency);
+	__declspec(dllexport) void MakeGabor16Filters(float* filter, int angleNum, float frequency);
+	__declspec(dllexport) void MakeGabor32Filters(float* filter, int angleNum, float frequency);
 }
 
 __global__ void cudaCreateGaborFilter(CUDAArray<float> filters, int size, float frequency, float bAngle)
 {
-	float aCos = cos(M_PI / 2 + bAngle * (blockIdx.x));
-	float aSin = sin(M_PI / 2 + bAngle * (blockIdx.x));
+	int column = defaultColumn();
+
+	float aCos = cos(CUDART_PI_F / 2 + bAngle * (blockIdx.x));
+	float aSin = sin(CUDART_PI_F / 2 + bAngle * (blockIdx.x));
 
 	int center = size / 2;
-	int upperCenter = (size & 1) == 1 ? center - 1 : center;
+	int upperCenter = (size & 1) == 0 ? center - 1 : center;
 
-	for (int i = -upperCenter; i < center; i++)
+	if (16 > column)
 	{
 		for (int j = -upperCenter; j < center; j++)
 		{
-			filters.SetAt(blockIdx.x * blockDim.x + center - i , center - j, exp(-0.5 * ((i * aSin + j * aCos) * (i * aSin + j * aCos) / 16 + (-i *aCos + j * aSin) * (-i *aCos + j * aSin) / 16)) * cos(2 * M_PI * (i * aSin + j * aCos) * frequency));
+			int i = column - center;
+			int row = blockDim.x * blockIdx.x + center + j - 1;
+
+			float xDash = i * aSin + j * aCos;
+			float yDash = -i *aCos + j * aSin;
+			float cellExp = exp(-0.5 * (xDash * xDash / 16 + yDash * yDash / 16));
+			float cellCos = cos(2 * CUDART_PI_F * xDash * frequency);
+
+			filters.SetAt(row, column, cellExp * cellCos);
 		}
 	}
 }
 
-void MakeGaborFilters(float* filter, int size, int angleNum, float frequency)
+void MakeGabor16Filters(float* filter, int angleNum, float frequency)
 {
-	CUDAArray<float> filters = CUDAArray<float>(size, size * angleNum);
+	CUDAArray<float> filters = CUDAArray<float>(16, 16 * angleNum);
 	
-	float bAngle = (float) M_PI / angleNum;
+	float bAngle = (float) CUDART_PI_F / angleNum;
+
+	dim3 blockSize = dim3(16 * 16);
+	dim3 gridSize = dim3(angleNum);
+
+	cudaCreateGaborFilter << < gridSize, blockSize >> > (filters, 16, frequency, bAngle);
+
+	filters.GetData(filter);
+}
+
+void MakeGabor32Filters(float* filter, int angleNum, float frequency)
+{
+	CUDAArray<float> filters = CUDAArray<float>(32, 32 * angleNum);
+
+	float bAngle = (float)CUDART_PI_F / angleNum;
 
 	dim3 blockSize = dim3(defaultThreadCount);
 	dim3 gridSize = dim3(ceilMod(angleNum, defaultThreadCount));
 
-	cudaCreateGaborFilter << <gridSize, blockSize >> > (filters, size, frequency, bAngle);
+	cudaCreateGaborFilter << <gridSize, blockSize >> > (filters, 32, frequency, bAngle);
 
 	filters.GetData(filter);
+}
+
+int main()
+{
+	float* b = (float*)malloc(16*16*8*sizeof(float));
+
+	MakeGabor16Filters(b, 8, (float) 1 / 9);
+
+	return 0;
 }
--- Code/CUDAFingerprinting.GPU.Normalization.Test/GaborFiltersTest.cs
+++ Code/CUDAFingerprinting.GPU.Normalization.Test/GaborFiltersTest.cs
@@ -8,15 +8,17 @@ namespace CUDAFingerprinting.GPU.Tests
     [TestClass]
     public class GaborFiltersTest
     {
-        [DllImport("CUDAFingerprinting.GPU.Filters.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "MakeGaborFilters")]
-        public static extern void MakeGaborFilters(float[] filter, int size, int angleNum, float frequency);
+        [DllImport("CUDAFingerprinting.GPU.Filters.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "MakeGabor16Filters")]
+        public static extern void MakeGabor16Filters(float[] filter, int angleNum, float frequency);
+        [DllImport("CUDAFingerprinting.GPU.Filters.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "MakeGabor32Filters")]
+        public static extern void MakeGabor32Filters(float[] filter, int angleNum, float frequency);
 
         [TestMethod]
-        public void CreateGaborFilter0Test()
+        public void CreateGaborFilter16Test()
         {
             var filters = new float[16 * 16 * 8];
 
-            MakeGaborFilters(filters, 16, 8, (float) 1 / 9);
+            MakeGabor16Filters(filters, 8, (float) 1 / 9);
 
             ImageHelper.SaveArray(filters.Make2D(16 * 8, 16), "test.bmp", true);
         }
