e52876b28c28ffcc95777c57f9a0b0ad7c0de420
Tarasenko Nikita <nik.d.tarasenko@gmail.com>
2015-07-23 15:49:31 +0300

    Added methods: creator of section, locate a max, calculate new point after step.

---
+++ Code/CUDAFingerprinting.RidgeLine/Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CUDAFingerprinting.Common;
+
+namespace CUDAFingerprinting.RidgeLine
+{
+    class Program
+    {
+        static void TestSection()
+        {
+            var image = Resources.SampleFinger2;
+            var array = ImageHelper.LoadImageAsInt(image);
+
+            int wing = 8;
+
+            RidgeLine newProssess = new RidgeLine(array, 2, wing);
+
+            newProssess.NewSelection(210, 180);
+
+            for (int i = 0; i < wing * 2 + 1; i++)
+            {
+                Console.Write("{0} ", newProssess._section[i]);
+            }
+            Console.WriteLine("\n{0}", array[210, 180]);
+            Console.WriteLine("{0}", newProssess.FindNextMax());
+
+            var newImage = new int[2 * wing + 1, 255];
+            Array.Clear(newImage, 0, newImage.Length);
+
+            for (int i = 0; i < 2 * wing + 1; i++)
+            {
+                int x = newProssess._section[i]/1000;
+                int y = newProssess._section[i]%1000;
+
+                for (int j = 0; j < array[x, y]; j++)
+                {
+                    newImage[i, j] = 120;
+                }
+            }
+
+            ImageHelper.SaveArray(newImage, "testSections.bmp");
+        }
+
+        static void Main()
+        {
+            TestSection();
+        }
+    }
+}
---
+++ Code/CUDAFingerprinting.RidgeLine/RidgeLine.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CUDAFingerprinting.Common;
+
+namespace CUDAFingerprinting.RidgeLine
+{
+    class RidgeLine
+    {
+        private int[,] _image;
+        private OrientationField _orientation;
+        private int _step;
+        private int _wings;
+        private const int BuildUp = 1000;  //specially for trade coordinates between methods
+
+        public int[] _section;
+
+        public RidgeLine(int[,] image, int step, int wings)
+        {
+            _image = image;
+            _orientation = new OrientationField(image);
+            _step = step;
+            _wings = wings;
+            _section = new int[2*wings + 1];
+        }
+
+        public void NewSelection(int mid)
+        {
+            int i = mid/BuildUp;
+            int j = mid%BuildUp;
+
+            double angle = _orientation.GetOrientation(i, j) + Math.PI / 2;
+
+            _section[_wings] = i * BuildUp + j;
+
+            for (var k = 1; k <= _wings; k++)
+            {
+                int xs, ys, xe, ye;
+                xs = Convert.ToInt32(i - k * Math.Cos(angle));
+                ys = Convert.ToInt32(j - k * Math.Sin(angle));
+                xe = Convert.ToInt32(i + k * Math.Cos(angle));
+                ye = Convert.ToInt32(j + k * Math.Sin(angle));
+
+                _section[_wings - k] = xs * BuildUp + ys;
+                _section[_wings + k] = xe * BuildUp + ye;
+            }
+        }
+
+        public int FindNextMax()
+        {
+            int loc = 0;
+            int i = 1;
+
+            while (loc == 0)
+            {
+                int x = _section[_wings - i] / BuildUp;
+                int y = _section[_wings - i] % BuildUp;
+
+                if (_image[x, y] == 255)
+                {
+                    loc = _section[i];
+                }
+                else
+                {
+                    x = _section[_wings + i] / BuildUp;
+                    y = _section[_wings + i] % BuildUp;
+
+                    if (_image[x, y] == 255)
+                    {
+                        loc = _section[i];
+                    }
+                }
+
+                i++;
+            }
+
+            return loc;
+        }
+
+        int MakeStep(int startPoint)
+        {
+            int x = startPoint/BuildUp;
+            int y = startPoint%BuildUp;
+
+            double angle = _orientation.GetOrientation(x, y);
+
+            x += Convert.ToInt32(_step * Math.Cos(angle));
+            y += Convert.ToInt32(_step * Math.Sin(angle));
+
+            return x*BuildUp + y;
+        }
+
+        bool CheckofStopCriteria()
+        {
+            return true;
+        }
+
+        void GoToLine()
+        {
+            
+        }
+    }
+}
