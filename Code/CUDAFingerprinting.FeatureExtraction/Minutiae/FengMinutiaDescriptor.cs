dabdcd85a05a73ab439b43669b06db0b67b5df74
Denis Chusovitin <Chusovitin.Den@gmail.com>
2015-08-06 10:14:12 +0300

    Change CountMatching function

--- Code/CUDAFingerprinting.FeatureExtraction/Minutiae/FengMinutiaDescriptor.cs
+++ Code/CUDAFingerprinting.FeatureExtraction/Minutiae/FengMinutiaDescriptor.cs
@@ -15,37 +15,63 @@ namespace CUDAFingerprinting.FeatureExtraction.Minutiae
             Minutia[] desc = new Minutia[desc_.Length];
             Array.Copy(desc_, desc, desc.Length);
             float angle = point2.Angle - point1.Angle;
-            for(i = 0; i < desc.Length; i++)
+            for (i = 0; i < desc.Length; i++)
             {
-                desc[i].X = (desc[i].X - point1.X) * (int)Math.Cos(angle) + 
+                desc[i].X = (desc[i].X - point1.X) * (int)Math.Cos(angle) +
                             (desc[i].Y - point1.Y) * (int)Math.Sin(angle) + point2.X;
-                desc[i].Y = - (desc[i].X - point1.X) * (int)Math.Sin(angle) +
+                desc[i].Y = -(desc[i].X - point1.X) * (int)Math.Sin(angle) +
                             (desc[i].Y - point1.Y) * (int)Math.Cos(angle) + point2.Y;
                 desc[i].Angle -= angle;
             }
             return desc;
         }
 
-        private static Tuple<int, int> CountMatchings(Minutia[] desc1, Minutia[] desc2)
+        private static Tuple<int, int> CountMatchings(Minutia[] desc1, Minutia[] desc2, float radius, int height, int width)
         {
-            int m, M;
+            int m = 0, M = 0;
             int i, j;
             float eps = 0.1F;
+            bool isExist;
+            float magicConstant = 0.64F; //= 0.8 * 0.8  0.8 is magic constant too!
+
             for (i = 0; i < desc1.Length; i++)
             {
                 for (j = 0; j < desc2.Length; i++)
                 {
+                    isExist = false;
 
+                    if ((desc1[i].X == desc2[j].X) && (desc1[i].Y == desc2[j].Y)
+                        && (Math.Abs(desc1[i].Angle - desc2[j].Angle) < eps))
+                    {
+                        isExist = true;
+                    }
+
+                    if (isExist)
+                    {
+                        ++m;
+                        ++M;
+                    }
+                    else
+                    {
+                        if ((Math.Abs(desc1[i].X - desc2[j].X) +
+                            Math.Abs(desc1[i].Y - desc2[j].Y) < magicConstant * radius * radius) &&
+                            (desc1[i].X > 0 && desc1[i].Y < width && desc1[i].Y > 0 && desc1[i].Y < height))
+                        {
+                            ++M;
+                        }
+                    }
                 }
             }
+
+            return Tuple.Create(m, M);
         }
 
-        public static float MinutiaCompare(Minutia[] desc1, Minutia point1, Minutia[] desc2, Minutia point2, float radius)
+        public static float MinutiaCompare(Minutia[] desc1, Minutia point1, Minutia[] desc2, Minutia point2, float radius, int height, int width)
         {
             Minutia[] tempdesc = Transformate(desc1, point1, point2);
 
 
-                    return 0.0F;          
+            return 0.0F;
         }
     }
 }
