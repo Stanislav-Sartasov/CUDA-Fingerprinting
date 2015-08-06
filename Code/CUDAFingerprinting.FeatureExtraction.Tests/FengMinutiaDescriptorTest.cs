8c35c42100a2e28a01462c0b55dafb2e86748055
Nik <borovikow.nikita@gmail.com>
2015-08-06 12:12:43 +0300

    Init commit for FengMinutiaDescriptor test

---
+++ Code/CUDAFingerprinting.FeatureExtraction.Tests/FengMinutiaDescriptorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using CUDAFingerprinting.Common;
+using CUDAFingerprinting.FeatureExtraction.SingularPoints;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CUDAFingerprinting.FeatureExtraction.Tests
+{
+    [TestClass]
+    public class FengMinutiaDescriptorTest
+    {
+        [TestMethod]
+        public void FengMinutiaDescriptorTestMethod()
+        {
+
+        }
+    }
+}
