86557be10cf847bd0995930e3617f9a898478eb7
puninvv <punin.v.v@gmail.com>
2015-08-10 15:07:21 +0300

    Added class DelaunayTriangulation.cs, and tests

---
+++ Code/CUDAFingerprinting.ImageProcessing.Tests/DelaunayTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CUDAFingerprinting.ImageProcessing.Triangulation;
+
+namespace CUDAFingerprinting.ImageProcessing.Tests
+{
+    [TestClass]
+    public class DelaunayTest
+    {
+        [TestMethod]
+        public void TestTriangulation()
+        {
+            List<DelaunayTriangulation.Point> points = new List<DelaunayTriangulation.Point>();
+            points.Add(new DelaunayTriangulation.Point(0,0));
+            points.Add(new DelaunayTriangulation.Point(0,2));
+            points.Add(new DelaunayTriangulation.Point(0,-2));
+            points.Add(new DelaunayTriangulation.Point(2,1));
+            points.Add(new DelaunayTriangulation.Point(2,-1));
+            points.Add(new DelaunayTriangulation.Point(-2,1));
+            points.Add(new DelaunayTriangulation.Point(-2,-1));
+
+            List<DelaunayTriangulation.Triangle> triangles = DelaunayTriangulation.CreateDelaunayTriangulation(points);
+        }
+    }
+}
