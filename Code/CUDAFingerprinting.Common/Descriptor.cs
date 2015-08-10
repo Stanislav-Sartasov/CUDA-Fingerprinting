41a727c93dcdc90cc415a97f1a1fa83d8363f809
Nik <borovikow.nikita@gmail.com>
2015-08-10 15:27:30 +0300

    List of Descriptors builder added

--- Code/CUDAFingerprinting.Common/Descriptor.cs
+++ Code/CUDAFingerprinting.Common/Descriptor.cs
@@ -14,4 +14,3 @@ namespace CUDAFingerprinting.Common
         }
     };
 }
-
---
+++ Code/CUDAFingerprinting.FeatureExtraction/Minutiae/DescriptorBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CUDAFingerprinting.Common;
+
+namespace CUDAFingerprinting.FeatureExtraction.Minutiae
+{
+    class DescriptorBuilder
+    {
+        private float leng(Minutia p1, Minutia p2)
+        {
+            float length;
+            length = (float)(Math.Pow(p1.X - p2.X, 2.0) +
+                             Math.Pow(p1.Y - p2.Y, 2.0));
+            return length;
+        }
+
+        public List<Descriptor> BuildDescriptors(List<Minutia> list, int radius)
+        {
+            List<Descriptor> desc = new List<Descriptor>();
+            int i, j;
+            float length;
+            for (i = 0; i < list.Count; i++)
+            {
+                for (j = i + 1; j < list.Count; j++)
+                {
+                    length = leng(list[i], list[j]);
+                    if (length <= radius)
+                    {
+                        desc[i].Minutias.Add(list[j]);
+                    }
+                }
+            }
+            return desc;
+        }
+    }
+}
