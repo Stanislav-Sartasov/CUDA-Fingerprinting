cd754fb0f7e406e337c1486122da0a65622ec6f7
Nik <borovikow.nikita@gmail.com>
2015-08-13 14:19:49 +0300

    First try of marshalling

--- Code/CUDAFingerprinting.Common/Descriptor.cs
+++ Code/CUDAFingerprinting.Common/Descriptor.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace CUDAFingerprinting.Common
 {
+//    [StructLayout(LayoutKind.Sequential)]
     public struct Descriptor
     {
+//        [MarshalAs(UnmanagedType.LPArray)]
         public List<Minutia> Minutias;
+//        [MarshalAs(UnmanagedType.Struct)]
         public Minutia Center;
         public Descriptor(List<Minutia> m, Minutia c)
         {
