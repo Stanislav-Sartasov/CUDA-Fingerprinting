651b26399eba81a818da34f1713ee3182fcc2e6d
Nik <borovikow.nikita@gmail.com>
2015-08-20 13:17:00 +0300

    preAlpha versions of fingerprints reader and DescriptorBuilder

--- Code/CUDAFingerprinting.GPU.FengMinutiaDescriptor/MinutiaHelper.cu
+++ Code/CUDAFingerprinting.GPU.FengMinutiaDescriptor/MinutiaHelper.cu
@@ -1,6 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include "MinutiaHelper.cuh"
+#include "Constants.cuh"
 
 __device__ float sqrLength(Minutia m1, Minutia m2)
 {
 	return (float)((m1.x - m2.x)*(m1.x - m2.x) + (m1.y - m2.y)*(m1.y - m2.y));
 }
+
+__global__ void fingerRead (char *dbPath, int dbSize, Minutia **mins, int *minutiaNum)
+{
+	FILE *finger;
+	int pathLength = 100;
+	char filePath[1] = "";
+	int num = blockIdx.x;
+	char fileNum[FILENAME_LENGTH];
+	int i;
+
+	if (num >= dbSize)
+	{
+		return;
+	}
+
+	/*itoa(num, fileNum, 10);
+	strncat(filePath, dbPath, FILENAME_LENGTH);
+	strncat(filePath, fileNum, FILENAME_LENGTH);
+	strncat(filePath, ".txt", FILENAME_LENGTH);
+
+	finger = fopen(dbPath, "r");
+
+	fscanf(finger, "%d", minutiaNum[num]);
+	for (i = 0; i < *minutiaNum; i++)
+	{
+		fscanf(finger, "%d %d %f", mins[num][i].x, mins[num][i].y, mins[num][i].angle);
+	}
+
+	fclose(finger);
+*/}
--- Code/CUDAFingerprinting.GPU.FengMinutiaDescriptor/MinutiaHelper.cuh
+++ Code/CUDAFingerprinting.GPU.FengMinutiaDescriptor/MinutiaHelper.cuh
@@ -5,4 +5,6 @@
 
 __device__ float sqrLength(Minutia m1, Minutia m2);
 
+__global__ void fingerRead(char *dbPath, int dbSize, Minutia **mins, int *minutiaNum);
+
 #endif
