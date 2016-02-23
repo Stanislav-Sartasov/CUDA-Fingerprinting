d8c96ce4f9f7de17aaa8ce6819d10c11abed7152
SeriousCoder <0kintol0@gmail.com>
2016-02-23 14:55:31 +0300

    - Done first version of the new algorithm.
    - Rewrited Paint function.

--- Code/CUDAFingerprinting.RidgeLine/RidgeOnLine.cs
+++ Code/CUDAFingerprinting.RidgeLine/RidgeOnLine.cs
@@ -18,7 +18,7 @@ namespace CUDAFingerprinting.RidgeLine
         private List<Tuple<Minutia, MinutiaTypes>> _minutias;
         private int[,] _image;
         private PixelwiseOrientationField _orientation;
-        private bool[,] _visited;
+        public bool[,] _visited;  //after tests must be private
         private int _step;
         private int _lengthWings;
         private bool _diffAngle;
@@ -54,10 +54,16 @@ namespace CUDAFingerprinting.RidgeLine
 
         private void NewSection(Tuple<int, int> point)  //maybe need remake this func...
         {
-            _sectionsCenter = 2 * _lengthWings; //At the end func this var will be division in 2 for save section's center
+            for (int index = 0; index < _section.Length; index++)
+            {
+                _section[index] = new Tuple<int, int>(-1, -1);
+            }
 
             int x = point.Item1;
             int y = point.Item2;
+
+            int lEnd = _lengthWings;
+            int rEnd = lEnd;
 
             bool rightE = false;
             bool leftE = false;
@@ -73,23 +79,20 @@ namespace CUDAFingerprinting.RidgeLine
                 int xe = Convert.ToInt32(x + i * Math.Cos(angle));
                 int ye = Convert.ToInt32(y + i * Math.Sin(angle));
 
-                _section[_lengthWings + i] = new Tuple<int, int>(-1, -1);
-                _section[_lengthWings - i] = new Tuple<int, int>(-1, -1);
-
-
                 if (!OutOfImage(xs, ys) && _image[ys, xs] < 15 && !rightE)
                 {
                     _section[_lengthWings - i] = new Tuple<int, int>(xs, ys);
+                    rEnd--;
                 }
                 else
                 {
                     rightE = true;
-                    _sectionsCenter -= i;
                 }
                 
-                if (!OutOfImage(xe, ye) && _image[ys, xs] < 15 && !leftE)
+                if (!OutOfImage(xe, ye) && _image[ye, xe] < 15 && !leftE)
                 {
                     _section[_lengthWings + i] = new Tuple<int, int>(xe, ye);
+                    lEnd++;
                 }
                 else
                 {
@@ -97,7 +100,7 @@ namespace CUDAFingerprinting.RidgeLine
                     _sectionsCenter += i;
                 }
 
-                _sectionsCenter /= 2;
+                _sectionsCenter = (lEnd + rEnd) / 2;
 
                 x = _section[_sectionsCenter].Item1;
                 y = _section[_sectionsCenter].Item2;
@@ -111,24 +114,26 @@ namespace CUDAFingerprinting.RidgeLine
             return x < 0 || y < 0 || x >= _image.GetLength(1) || y >= _image.GetLength(0);
         }
 
-        private Tuple<int, int> MakeStep()
+        private Tuple<int, int> MakeStep(int _x = -1, int _y = -1, bool forPaint = false)
         {
-            int x = _section[_sectionsCenter].Item1;
-            int y = _section[_sectionsCenter].Item2;
+            int x = forPaint ? _x : _section[_sectionsCenter].Item1;
+            int y = forPaint ? _y : _section[_sectionsCenter].Item2;
+
+            int __step = forPaint ? 1 : _step;
 
             double angle = _sectionsAngle + ((int)_direction) * Math.PI + (_diffAngle ? Math.PI : 0) + Math.PI * 2;
             while (angle > Math.PI * 2) angle -= Math.PI * 2;
 
-            double dx = x + _step * Math.Cos(angle);
-            double dy = y + _step * Math.Sin(angle);
+            double dx = x + __step * Math.Cos(angle);
+            double dy = y + __step * Math.Sin(angle);
 
             x = (int)(dx >= 0 ? dx + 0.5 : dx - 0.5);
             y = (int)(dy >= 0 ? dy + 0.5 : dy - 0.5);
 
-            if (!OutOfImage(x, y)) return new Tuple<int, int>(-1, -1);
+            if (OutOfImage(x, y)) return new Tuple<int, int>(-1, -1);
 
             double ang2Check = _orientation.GetOrientation(x, y);
-            if ((_sectionsAngle * ang2Check < 0) ? (_sectionsAngle > 1.4) || (ang2Check > 1.4) : Math.Abs(_sectionsAngle - ang2Check) > 0.5)
+            if (((_sectionsAngle * ang2Check < 0) ? (_sectionsAngle > 1.4) || (ang2Check > 1.4) : Math.Abs(_sectionsAngle - ang2Check) > 0.5) & !forPaint)
             {
                 _diffAngle = !_diffAngle;
             }
@@ -138,19 +143,45 @@ namespace CUDAFingerprinting.RidgeLine
 
         private void Paint(Tuple<int, int>[] edge, int edgesCenter) //in process
         {
-            List<Tuple<int, int>> queue = new List<Tuple<int, int>> {edge[edgesCenter]};
+            List<Tuple<int, int>> queue = new List<Tuple<int, int>>();
+            List<Tuple<int, int>> stopPixels = new List<Tuple<int, int>>();
 
-            List<Tuple<int, int>> stopPixels = edge.Where(tuple => tuple.Item1 != -1).ToList();
-            stopPixels.AddRange(_section.Where(tuple => tuple.Item1 != -1));
+            foreach (var tuple in edge.Where(tuple => tuple.Item1 != -1))
+            {
+                _visited[tuple.Item2, tuple.Item1] = true;
+                queue.Add(tuple);
+                stopPixels.Add(tuple);
+            }
 
+            foreach (var tuple in _section.Where(tuple => tuple.Item1 != -1))
+            {
+                _visited[tuple.Item2, tuple.Item1] = true;
+                stopPixels.Add(tuple);
+            }
             while (queue.Count > 0)
             {
                 int x = queue[0].Item1;
                 int y = queue[0].Item2;
+                stopPixels.Add(queue[0]);
 
                 queue.RemoveAt(0);
 
-                
+                var pCheck = MakeStep(x, y, true);
+
+                for (int i = -1; i < 2; i++)
+                {
+                    for (int j = -1; j < 2; j++)
+                    {
+                        var fooPoint = new Tuple<int, int>(x + i, y + j);
+
+                        if (!OutOfImage(x + i, y + j) && _image[y + j, x + i] < 15 && !_visited[y + j, x + i] && !queue.Exists(q => (q.Item1 == fooPoint.Item1) 
+                                    && (q.Item2 == fooPoint.Item2)) && !stopPixels.Exists(q => (q.Item1 == fooPoint.Item1) && (q.Item2 == fooPoint.Item2)))
+                        {
+                            queue.Add(fooPoint);
+                            _visited[y + j, x + i] = true;
+                        }
+                    }
+                }
             }
         }
 
@@ -165,7 +196,7 @@ namespace CUDAFingerprinting.RidgeLine
                 return MinutiaTypes.Intersection;
             }
 
-            if (_image[y, x] < threshold)
+            if (_image[y, x] > threshold)
             {
                 MakeTestBmp(x, y);
                 return MinutiaTypes.LineEnding;
@@ -213,7 +244,12 @@ namespace CUDAFingerprinting.RidgeLine
                 y = point.Item2;
                 angle = _orientation.GetOrientation(x, y);
 
-                var oldSection = _section;
+                var oldSection = new Tuple<int, int>[9];
+                for (int i = 0; i < _section.Length; i++)
+                {
+                    oldSection[i] = _section[i];
+                }
+
                 var oldCenter = _sectionsCenter;
 
                 point = MakeStep();
@@ -227,7 +263,8 @@ namespace CUDAFingerprinting.RidgeLine
                 if (_section[_lengthWings].Item1 == -1) return; //?
 
                 minutiaType = CheckStopCriteria();
-                Paint(oldSection, oldCenter);
+                if (minutiaType == MinutiaTypes.NotMinutia) Paint(oldSection, oldCenter);
+                else minutiaType = minutiaType;
             } while (minutiaType == MinutiaTypes.NotMinutia);
 
             x = point.Item1;
@@ -241,7 +278,7 @@ namespace CUDAFingerprinting.RidgeLine
             return _minutias;
         }
 
-        public void FindMinutia(int x, int y, double duplicateDelta, int colorThreshold = 10)
+        public void FindMinutia(int x, int y, double duplicateDelta, int colorThreshold = 15)
         {
             if (_image[y, x] >= colorThreshold || _visited[y, x]) return;
             _visited[y, x] = true;
